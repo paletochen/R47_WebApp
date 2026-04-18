73def253baddf3bf9555b8b26d32fd7e2aa1c4ce
paletochen <davidcastro@google.com>
2026-04-19 01:20:09 +0200

    Generate a PWA to allow offline access

---
+++ sw.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = 'r47-calc-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/app.js',
+    '/favicon.png',
+    '/wasm/r47.js',
+    '/wasm/r47.wasm',
+    '/fonts/C47__StandardFont.ttf',
+    '/fonts/C47__NumericFont.ttf',
+    '/fonts/C47__TinyFont.ttf'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Service Worker: Caching Assets');
+                return cache.addAll(ASSETS);
+            })
+            .then(() => self.skipWaiting())
+    );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        console.log('Service Worker: Clearing Old Cache');
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch Events
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
